40c67ffd4fe3a1f6dfa4d0ebd441f9eb15802c34
Vinod Rao <vrao1@hawk.iit.edu>
2017-12-26 12:41:00 -0600

    Unused File

--- others/a.php
+++
@@ -1,9 +0,0 @@
-<html>
-<form action="b.php" method=post>
-<?php
-	$array_var = array("red" => 1, "green" => 2);
-?>
-<input type="hidden" id="str_var" name="str_var" value="<?php print base64_encode(serialize($array_var)) ?>" >
-<input type="submit" value="Submit" >
-</form>
-</html>
